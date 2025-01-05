e80b8ca393a6a35ae56cfd43ce94b44f0279d239
たいち ひ <taichi221228@icloud.com>
2025-01-05 16:39:05 +0900

    feat: Add file download and save functionality
    Enable fetching a ZIP file from a URL and saving it locally using `writeFileSync`.

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
+import { writeFileSync } from "node:fs";
+
 console.log("Hello via Bun!");
+
+const response = await fetch(
+	"https://www.kaggle.com/api/v1/datasets/download/saurabhshahane/fake-news-classification",
+	{
+		headers: { "Content-Type": "application/json" },
+	},
+);
+
+const buffer = await response.arrayBuffer();
+const data = new Uint8Array(buffer);
+
+writeFileSync("fake-news-classification.zip", data);
+console.log("ZIP file saved!");
